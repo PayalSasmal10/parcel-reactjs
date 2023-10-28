26236b5d08f45287b80bb3e9cec1658f22cea677
Payalsasmal10 <sasmalpayal@gmail.com>
2023-10-28 22:27:47 +0530

    created parcel configuration

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const heading = React.createElement(
+    "h1",
+    {
+        id: "title",
+    },
+    "Heading 1"
+);
+
+const heading2 = React.createElement(
+    "h1",
+    {
+        id: "title",
+    },
+    "Heading 2"
+);
+
+const container =  React.createElement(
+    "div",
+    {
+        id :"container",
+        hello: "world",
+    },
+    [heading, heading2]
+);
+
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
